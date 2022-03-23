b0c66060d86e26d1a5530a018b5840f28abc3939
Artem Safin <artemsafin.webdev@gmail.com>
2022-03-24 02:19:40 +0300

    По селектам

--- src/js/customSelects.js
+++ src/js/customSelects.js
@@ -4,10 +4,29 @@ export default function customSelects() {
     const customSelects = Array.from(document.querySelectorAll('.js-custom-select'));
 
     customSelects.forEach((select) => {
-        new Choices(select, {
+        const parentForm = select.closest('form');
+        const instance = new Choices(select, {
             searchEnabled: false,
             itemSelectText: '',
             shouldSort: false,
         });
+
+        instance.passedElement.element.addEventListener(
+            'choice',
+            () => {
+                setTimeout(() => {
+                    $(instance.passedElement.element).parsley().validate();
+                }, 100);
+            },
+            false
+        );
+
+        if (parentForm) {
+            parentForm.addEventListener('reset', () => {
+                console.log('Parent form reset');
+
+                instance.setChoiceByValue(defaultValue);
+            });
+        }
     });
 }
